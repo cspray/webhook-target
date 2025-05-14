14a7cd64b26587ac07e7d687b10d6b442cc56b51
Charles Sprayberry <me@cspray.io>
2025-05-14 11:01:01 -0400

    Don't put all received webhooks into array structure

--- resources/templates/pages/dashboard/home.php
+++ resources/templates/pages/dashboard/home.php
@@ -5,25 +5,16 @@
 
 $this->layout('layouts::main');
 
-$webhooks = iterator_to_array($data->receivedWebhooks);
 
 ?>
 
 <section class="section">
     <div class="container is-max-tablet">
-        <?php if ($webhooks === []): ?>
-            <div class="message">
-                <div class="message-body">
-                    No webhooks have been received yet.
-                </div>
+        <?php foreach ($data->receivedWebhooks as $webhook): ?>
+            <div class="received-webhook mb-3">
+                <div>ID: <span class="has-text-weight-bold"><?= $this->e($webhook->id->toString()) ?></span></div>
+                <pre><code><?= $this->fetch('components::received-webhook', ['webhook' => $webhook]) ?></code></pre>
             </div>
-        <?php else: ?>
-            <?php foreach ($webhooks as $webhook): ?>
-                <div class="received-webhook mb-3">
-                    <div>ID: <span class="has-text-weight-bold"><?= $this->e($webhook->id->toString()) ?></span></div>
-                    <pre><code><?= $this->fetch('components::received-webhook', ['webhook' => $webhook]) ?></code></pre>
-                </div>
-            <?php endforeach ?>
-        <?php endif ?>
+        <?php endforeach ?>
     </div>
 </section>
