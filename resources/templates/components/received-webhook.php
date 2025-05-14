934a53a5c815a48b477175488dd36e1900fe1c61
Charles Sprayberry <me@cspray.io>
2025-05-14 09:39:23 -0400

    Properly escape webhook data, pretty print json data

--- resources/templates/components/received-webhook.php
+++ resources/templates/components/received-webhook.php
@@ -3,13 +3,20 @@
 /** @var \Cspray\WebhookTarget\WebhookTarget\Entity\ReceivedWebhook $webhook */
 
 ?>
-<?= $webhook->method ?> <?= $webhook->uri->getPath() ?> HTTP/<?= $webhook->protocol ?>
+<?= $this->e($webhook->method) ?> <?= $this->e($webhook->uri->getPath()) ?> HTTP/<?= $this->e($webhook->protocol) ?>
 
 <?php foreach ($webhook->headers as $header => $values): ?>
 <?php foreach ($values as $value): ?>
-<?= $header ?>: <?= $value ?>
+<?= $this->e($header) ?>: <?= $this->e($value) ?>
 
 <?php endforeach ?>
 <?php endforeach ?>
 
-<?= $webhook->body ?>
+
+<?php
+
+$json = json_decode($webhook->body, true, flags: JSON_THROW_ON_ERROR);
+$body = json_encode($json, flags: JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+
+?>
+<?= $this->e($body) ?>
